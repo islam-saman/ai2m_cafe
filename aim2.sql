65fad0af369f288541d030407d577c16c995acda
islam-saman <islam.saman4@gmail.com>
2023-04-26 16:56:36 +0200

    building the project structure

---
+++ aim2.sql
@@ -0,0 +1,145 @@
+CREATE DATABASE  IF NOT EXISTS `aim2` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+USE `aim2`;
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: aim2
+-- ------------------------------------------------------
+-- Server version	5.7.42-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order`
+--
+
+DROP TABLE IF EXISTS `order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `room` varchar(45) COLLATE utf8_bin NOT NULL,
+  `ext` int(11) NOT NULL,
+  `status` enum('processing','out for delivery','done') COLLATE utf8_bin NOT NULL DEFAULT 'processing',
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `user_id_idx` (`user_id`),
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order`
+--
+
+LOCK TABLES `order` WRITE;
+/*!40000 ALTER TABLE `order` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) COLLATE utf8_bin NOT NULL,
+  `price` int(11) NOT NULL,
+  `image` varchar(45) COLLATE utf8_bin NOT NULL,
+  `is_available` tinyint(1) DEFAULT NULL,
+  `category_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `category_id_idx` (`category_id`),
+  CONSTRAINT `category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) COLLATE utf8_bin NOT NULL,
+  `email` varchar(45) COLLATE utf8_bin NOT NULL,
+  `password` varchar(45) COLLATE utf8_bin NOT NULL,
+  `room_no` int(11) NOT NULL,
+  `ext` int(11) NOT NULL,
+  `secret_key` varchar(45) COLLATE utf8_bin NOT NULL,
+  `is_admin` tinyint(4) NOT NULL,
+  `profile_picture` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id_UNIQUE` (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-04-25 18:49:17
---
+++ helpers/database.php
@@ -0,0 +1,180 @@
+<?php 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+
+class Database
+{
+    private string $dbHost;
+    private int $dbPort;
+    private string $dbUser;
+    private string $dbPass;
+    private string $dbName;
+    private $dbConnection;
+
+    function __construct(string $dbHost, int $dbPort, string $dbUser, string $dbPass, string $dbName)
+    {
+        $this->dbHost = $dbHost;
+        $this->dbPort = $dbPort;
+        $this->dbUser = $dbUser;
+        $this->dbPass = $dbPass;
+        $this->dbName = $dbName;
+    }
+    private function connect()
+    {
+        try
+        {
+            $dbURL = "mysql:dbname=$this->dbName;host=$this->dbHost;port=$this->dbPort";
+            $this->dbConnection = new PDO($dbURL, $this->dbUser, $this->dbPass);   
+            return $this->dbConnection;
+        }
+        catch(Exception $dbConError)
+        {
+            echo $dbConError->getMessage();
+        }
+    }
+
+
+    public function fetchALl($tableName)
+    {
+        if (!$this->connect())
+            throw new Exception;
+        
+
+        $selectQuery = "SELECT * FROM $tableName";
+        $selectStatement = $this->dbConnection->prepare($selectQuery);
+        $selectStatement->execute();        
+
+        if($selectStatement->rowCount() != 0)
+            return $selectStatement->fetchALl(PDO::FETCH_ASSOC);
+        else
+            return [];     
+    }
+
+
+    // If we have time, we must let the user of the function to decide the desired fields
+    public function  fetchOne(string $tableName, string $primaryKey, string $value)
+    {
+        if (!$this->connect())
+            throw new Exception;
+
+        $selectQuery = "SELECT * FROM $tableName where $primaryKey = '{$value}'";
+        $selectStatement = $this->dbConnection->prepare($selectQuery);
+        $selectStatement->execute();        
+
+        if($selectStatement->rowCount() != 0)
+            return $selectStatement->fetch(PDO::FETCH_ASSOC);
+        else
+            return null;
+
+    }
+
+
+    public function insert(string $tableName, array $columns, array $columnsValue)
+    {
+        if (!$this->connect())
+            throw new Exception;
+
+        $rowColumns = "";
+        foreach($columns as $column)
+        {
+            if($rowColumns == "")
+                $rowColumns .= $column;
+            else
+                $rowColumns .= ",".$column;
+        }
+
+        // we could achive this in just one for loop, but I sepreate them to be very clear for me when reading the code again
+        $rowValues = "";
+        foreach($columnsValue as $value)
+        {
+            if($rowValues == "")
+                $rowValues .= "'{$value}'";
+            else
+                $rowValues .= ","."'{$value}'";
+        }
+
+        $insrtQuery = "INSERT INTO users ($rowColumns) values ($rowValues)";
+        $insetStatement = $this->dbConnection->prepare($insrtQuery);
+        $insetStatement->execute();
+        
+        if($insetStatement->rowCount())
+            return true;
+        else
+            return false;
+    }
+
+
+    public function updateById(string $tableName, array $columns, array $columnsValue, string $id)
+    {
+        if (!$this->connect())
+            throw new Exception;
+
+        $prepareSet = "";
+        for($index = 0; $index < count($columns); $index++)
+        {
+            if($index === 0)
+            {
+                $prepareSet .= "$columns[$index] = '$columnsValue[$index]'";
+            }
+            else
+            {
+                $prepareSet .= ","."$columns[$index] = '$columnsValue[$index]'";   
+            }
+        }
+        echo $prepareSet;
+
+
+        $updateQuery = "UPDATE users SET $prepareSet WHERE id=$id";
+        $updateStatement = $this->dbConnection->prepare($updateQuery);
+        $updateStatement->execute();
+
+        if($updateStatement->rowCount())
+            return true;
+        else
+            return false;
+    }
+
+    // be carfull ... since the key here may not be primary then it may delete more than one recored
+    public function deleteOne(string $tableName, string $primayKey, string $value)
+    {
+        if (!$this->connect())
+            throw new Exception;
+
+        $deleteQuery = "DELETE FROM $tableName where $primayKey = '$value'";
+        $deleteStatement = $this->dbConnection->prepare($deleteQuery);
+        $deleteStatement->execute();
+
+        if($deleteStatement->rowCount() != 0)
+            return true;
+        else
+            return false;   
+
+    }
+
+
+
+    public function isExisted(string $tableName, string $primaryKey, string $value)
+    {
+        if (!$this->connect())
+            throw new Exception;
+
+        // check of the this user is already exist
+        $selectQuery = "SELECT $primaryKey FROM $tableName where $primaryKey = '{$value}'";
+        $selectStatement = $this->dbConnection->prepare($selectQuery);
+        $selectStatement->execute();
+
+        if($selectStatement->rowCount() != 0)
+            return true;
+        else
+            return false;
+        
+    }
+
+
+}
+
+?>
